26affc402293bd04f9456678eb250e1e6ecf27f5
Johannes Stein <johannesstein@freeze-dev.de>
2011-04-23 05:52:48 +0200

    Added spikes and SendMessage for Chainsaw

--- Assets/Code/Behaviour/Chainsaw.cs
+++ Assets/Code/Behaviour/Chainsaw.cs
@@ -6,28 +6,27 @@ public class Chainsaw : MonoBehaviour {
 	public float speed = 1.0f;
 
 	private Vector3 startPos;
-	
+
 	// Use this for initialization
 	void Start () {
-		
+
 		// Set start position
 		startPos = transform.position;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 
 		// Check if deltaPos has values and then apply interpolation
         transform.position = Vector3.Lerp(startPos, startPos + deltaPos, Mathf.PingPong(Time.time * speed, 1));
-		
+
 		// TODO: Rotation
 		//Quaternion rotation = Quaternion.AngleAxis(3f, Vector3.forward);
-		//transform.rotation *= rotation; 
-		
+		//transform.rotation *= rotation;
+
 	}
-	
-	void OnCollisionEnter(Collision collision)
-	{
-		if (collision.gameObject == GameObject.Find("Player")) Debug.Log("Insta-Death");
+
+	void OnTriggerEnter(Collider other) {
+		other.SendMessage("Die", SendMessageOptions.DontRequireReceiver);
 	}
 }
