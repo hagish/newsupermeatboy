27bc40278c7901380c06cd4e1190b1fe269df7df
Johannes Stein <johannesstein@freeze-dev.de>
2011-04-23 06:28:20 +0200

    Added Bandage girl base

---
+++ Assets/Code/Behaviour/BandageGirl.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class BandageGirl : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// TODO: Play animation if available
+		
+	}
+	
+	void OnTriggerEnter(Collider other) 
+	{
+		other.SendMessage("GameWon", SendMessageOptions.DontRequireReceiver);
+		
+		// Play Winning animation
+	}
+}
