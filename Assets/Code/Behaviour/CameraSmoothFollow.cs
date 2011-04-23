1ef1c54f478679c7aa73bd205401fe90353acd20
ghoulsblade <ghoulsblade@schattenkind.net>
2011-04-23 04:09:20 +0200

    smooth cam

--- Assets/Code/Behaviour/CameraSmoothFollow.cs
+++ Assets/Code/Behaviour/CameraSmoothFollow.cs
@@ -13,13 +13,14 @@ public class CameraSmoothFollow : MonoBehaviour {
 	void Update () {
 		Vector3 viewPos = camera.WorldToViewportPoint(target.transform.position);
 		
+		/*
 		float border = 0.1f;
 		float speed = 1.0f;
 			
 		// out of visible 
 		if (
-		    viewPos.x < 0.0f + border || viewPos.x > 1.0f - border || 
-		    viewPos.y < 0.0f + border || viewPos.y > 1.0f - border
+		    viewPos.x < 0.4f + border || viewPos.x > 0.6f - border || 
+		    viewPos.y < 0.4f + border || viewPos.y > 0.6f - border
 		)
 		{
 			Vector3 fromTargetToCam = target.transform.position - transform.position;
@@ -30,5 +31,11 @@ public class CameraSmoothFollow : MonoBehaviour {
 			
 			transform.position += fromTargetToCam;
 		}	
+		*/
+		float f = 0.04f;
+		float fi = 1f - f;
+			Vector3 newcampos = fi* transform.position + f* target.transform.position;
+			newcampos.z = transform.position.z;
+			transform.position = newcampos;
 	}
 }
