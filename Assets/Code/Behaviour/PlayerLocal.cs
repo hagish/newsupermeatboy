defbbbf0e504460a7ec1a6d5aa40a9c066ef0931
ghoulsblade <ghoulsblade@schattenkind.net>
2011-04-23 02:02:58 +0200

    wallslide + walljump first try (only works when slide, need trigger obj)

--- Assets/Code/Behaviour/PlayerLocal.cs
+++ Assets/Code/Behaviour/PlayerLocal.cs
@@ -11,51 +11,84 @@ phys	http://unity3d.com/support/documentation/Components/comp-DynamicsGroup.html
 printf : Debug.Log
 */
 
-public class PlayerLocal : Player {
+public class MainCharacter : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
 		transform.position = GameObject.Find("SpawnPoint").transform.position;
 	}
 	
-	/*
-	void OnCollisionEnter(Collision collision) {
-		foreach (ContactPoint contact in collision.contacts) {
-		//	Debug.DrawRay(contact.point, contact.normal, Color.white);
-		}
-		//if (collision.relativeVelocity.magnitude > 2)
+	bool	bTouchesWall = false;
+	bool	bTouchesWallLeft = false;
+	bool	bTouchesWallRight = false;
+	int		iTouchesWallXNormal = 0;
+	void	MyMoveInit	() {
+		bTouchesWall = false;
+		bTouchesWallLeft = false;
+		bTouchesWallRight = false;
+		iTouchesWallXNormal = 0;
 	}
-	*/
+	void OnControllerColliderHit(ControllerColliderHit hit) {
+		//Debug.Log("mainchar:OnControllerColliderHit");
+		if (Mathf.Abs(hit.moveDirection.x) < 0.3) return;
+		bTouchesWall = true;
+		if (hit.moveDirection.x < 0) { iTouchesWallXNormal =  1; bTouchesWallLeft = true; }
+		if (hit.moveDirection.x > 0) { iTouchesWallXNormal = -1; bTouchesWallRight = true; }
+	}
+	
+	// never called 
+	void OnCollisionEnter(Collision collision) { Debug.Log("mainchar:OnCollisionEnter"); }
+	
+	// never called 
+	void OnCollisionStay(Collision collisionInfo) { Debug.Log("mainchar:OnCollisionStay"); }
 
-public float speed = 6.0F;
-public float jumpSpeed = 8.0F;
-public float gravity = 20.0F;
+public static float speed = 8.0F;
+public static float myJumpSpeed = 10.0F;
+public static float gravity = 20.0F;
 private Vector3 moveDirection = Vector3.zero;
+bool bJumpBlocked = false;
 
 	// Update is called once per frame
 	void Update () {
-		float s = 2;
-		Vector3 vmove = Vector3.zero;
 		CharacterController controller = GetComponent<CharacterController>();
 		bool bJump = Input.GetKey("w") || Input.GetKey("up");
-		if (Input.GetKey("a") || Input.GetKey("left"))	vmove.x -= s;
-		if (Input.GetKey("d") || Input.GetKey("right"))	vmove.x += s;
-		//if (Input.GetKey("w") || Input.GetKey("up"))	vmove.y -= s;
-		//if (Input.GetKey("s") || Input.GetKey("down"))	vmove.y += s;
-		// Input.GetAxis("Horizontal")  Input.GetAxis("Vertical");
-		// transform.position += new Vector3(-s,0,0);
-		// rigidbody.AddForce(Vector3.left * s);
-		//float s = 0.1f * Time.deltaTime;
+		bool bLeft = Input.GetKey("a") || Input.GetKey("left");
+		bool bRight = Input.GetKey("d") || Input.GetKey("right");
 		
-	
+		// bJumpBlocked : bJump is only true for one frame
+		if (!bJump) bJumpBlocked = false;
+		if (bJumpBlocked) bJump = false;
+		if (bJump) bJumpBlocked = true;
+		
+		moveDirection.x = 0;
+		if (bLeft)	moveDirection.x -= speed;
+		if (bRight)	moveDirection.x += speed;
+		
 		if (controller.isGrounded) {
-			moveDirection = vmove;
-			if (bJump) moveDirection.y = jumpSpeed;
-		} else {
-			moveDirection.x = vmove.x;
+			//if (Input.GetKey("w") || Input.GetKey("up"))	moveDirection.y -= s;
+			//if (Input.GetKey("s") || Input.GetKey("down"))	moveDirection.y += s;
+			// Input.GetAxis("Horizontal")  Input.GetAxis("Vertical");
+			// transform.position += new Vector3(-s,0,0);
+			// rigidbody.AddForce(Vector3.left * s);
+			
+			if (bJump) moveDirection.y = myJumpSpeed;
 		}
-		moveDirection.y -= gravity * Time.deltaTime;
-		controller.Move(moveDirection * Time.deltaTime);
 		
+		float fGravFactor = 1;
+		
+		if (!controller.isGrounded && bTouchesWall) {
+			bool bWallSlide = (bTouchesWallLeft && bLeft) || (bTouchesWallRight && bRight);
+			if (bJump) {
+				moveDirection.x = 0.8f*myJumpSpeed*iTouchesWallXNormal;
+				moveDirection.y = 0.7f*myJumpSpeed;
+			}
+			if (moveDirection.y < 0f && bWallSlide) fGravFactor = 0.3f;
+		}
+		moveDirection.y -= gravity * fGravFactor * Time.deltaTime;
+		MyMoveInit();
+		controller.Move(moveDirection * Time.deltaTime);
+		if (bTouchesWall) {
+			
+		}
 	}
 }
