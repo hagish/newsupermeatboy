43531af7bc36e1afdc6a83da5883a610e7b7cfb7
Johannes Stein <johannesstein@freeze-dev.de>
2011-04-23 18:19:35 +0200

    Added different kinds of input support

--- Assets/Code/Behaviour/Game.cs
+++ Assets/Code/Behaviour/Game.cs
@@ -4,8 +4,6 @@ using System.Collections;
 public class Game : MonoBehaviour {
 	public static Game game;
 	
-	private int winningPlayerNr = 0;
-	
 	public Vector3 findSpawnPosition(int playerNr)
 	{
 		if (GameObject.Find("SpawnPoint" + playerNr) != null) return GameObject.Find("SpawnPoint" + playerNr).transform.position;
@@ -16,40 +14,35 @@ public class Game : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		game = this;
-		
-		// keynames see http://unity3d.com/support/documentation/Manual/Input.html
-		createLocalPlayer(1, "left", "right", "up");
-		createLocalPlayer(2, "a", "d", "w");
+
+        switch (Input.P1InputType)
+        {
+        	case "Arrow Keys": createLocalPlayer(1, KeyCode.LeftArrow, KeyCode.RightArrow, KeyCode.UpArrow); break;
+            case "Left/Right Arrow + Space": createLocalPlayer(1, KeyCode.LeftArrow, KeyCode.RightArrow, KeyCode.Space); break;
+            case "IJKL": createLocalPlayer(1, KeyCode.J, KeyCode.L, KeyCode.I); break;
+            case "Joystick/Gamepad 1": createLocalPlayer(2, KeyCode.Joystick1Button7, KeyCode.Joystick1Button8, KeyCode.Joystick1Button16); break;
+        }
+
+        switch (Input.P2InputType)
+        {
+        	case "WSAD": createLocalPlayer(2, KeyCode.A, KeyCode.D, KeyCode.W); break;
+            case "Joystick/Gamepad 2": createLocalPlayer(2, KeyCode.Joystick2Button7, KeyCode.Joystick2Button8, KeyCode.Joystick2Button16); break;
+        }
+
 	}
 	
-	public void playerFinished(int playerNr, Player player)
-	{
-		if (winningPlayerNr == 0)
-		{
-			winningPlayerNr = playerNr;	
-		}
-	}
-	
-	private void createLocalPlayer(int playerNr, string keyLeft, string keyRight, string keyJump)
+	private void createLocalPlayer(int playerNr, KeyCode keyLeft, KeyCode keyRight, KeyCode keyJump)
 	{
 		Vector3 position = findSpawnPosition(playerNr);
 		GameObject player = GameObjectHelper.createObject(gameObject, "Player", true, position, Quaternion.identity);
 		GameObject.Find("Camera").GetComponent<CameraSmoothFollow>().setTarget(playerNr, player);
 		player.AddComponent<PlayerLocal>().setKeyBindings(keyLeft, keyRight, keyJump);
 		player.GetComponent<Player>().playerNr = playerNr;
-		
-		/*
-		Material mat = (Material)Resources.Load("player" + playerNr);
-		
-		GameObjectHelper.visitComponentsDeep<SkinnedMeshRenderer>(player, (r) => {
-			r.material = mat;
-		});
-		*/
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown(KeyCode.Joystick1Button16)) Debug.Log(KeyCode.Joystick1Button16);
 	}
 	
 	void createNetworkPlayer ()
@@ -67,21 +60,5 @@ public class Game : MonoBehaviour {
 		Network.RemoveRPCs(player, 0);
 		Network.DestroyPlayerObjects(player);
 		Debug.Log("Player left from " + player.ipAddress + ":" + player.port);
-	}
-	
-	void OnGUI() {
-		if (GUI.Button(new Rect(5, 5, 100, 25), "back to menu"))
-	    {
-	    	Application.LoadLevel(0);
-	    }
-	
-		int x = (int)((float)Screen.width * 0.45f);
-		int y = (int)((float)Screen.height * 0.45f);
-		
-		if (winningPlayerNr > 0)
-		{
-			GUI.Label(new Rect(x, y, 200, 50), "player " + winningPlayerNr + " won!");
-			
-		}
 	}
 }
